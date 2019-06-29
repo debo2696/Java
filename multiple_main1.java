582603d3a8261019cf9aae1a2efc6438b8391047
Debajyoti Das <debo2696@gmail.com>
2019-06-29 22:50:05 +0530

    Create multiple_main1.java

---
+++ multiple_main1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class A
+{
+    public static void main(String[][][][] args) 
+    {
+        int ns=15,ns1=8,psvd;
+        String brands[]=new String[]{"Sony","IBM","Apple"};
+        B mainB=new B();
+        System.out.println("Debajyoti ");
+        mainB.main(brands);
+    }
+}
+class B
+{
+    static int bvar=1;   
+    public static void main(String[] args) 
+    {
+        String drinks[][][][]=new String[][][][]{{{{"Pepsi","Budweiser"}}}};
+        int i=0;
+        A mainA=new A();
+        System.out.println("Das");
+        mainA.main(drinks);      
+    }
+}
